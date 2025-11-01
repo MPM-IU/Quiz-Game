6f7b5f039c8e7f594dacfdb6c2eeb5b06830f071
Wookie6932 <byers6932@gmail.com>
2025-10-31 21:00:57 -0400

    Add individual GitHubLinks.java file for William Byers (wookie6932)
    Added my personal GitHubLinks.java file as part of the team setup assignment.
    Includes my GitHub account (wookie6932) and our shared project repository (MPM-IU/Quiz-Game).

---
+++ GitHubLinks.java
@@ -0,0 +1,12 @@
+/*
+ Team: MPM-IU Quiz-Game
+ Author: William Byers
+ Class: C211 / I211
+*/
+
+public class GitHubLinks {
+    public static void main(String[] args) {
+        System.out.println("My GitHub account: https://github.com/wookie6932");
+        System.out.println("Our project repository: https://github.com/MPM-IU/Quiz-Game");
+    }
+}
