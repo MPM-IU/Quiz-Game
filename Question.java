06a4bf4bfef04daa658651b2c65c07ce79ff2107
drewbusz <abusz@iu.edu>
2025-12-17 15:30:57 -0500

    Question.java

--- Question.java
+++ Question.java
@@ -4,9 +4,8 @@ package quizGame;
  * Project: Quiz Game
  * Author: Andrew Busz
  * Date: 12/4/2025
- * Description: This class represents a single quiz question,
- *             storing the question text, answer options, and
- *             the index of the correct answer.
+ * Description: Represents a single quiz question, including the
+ *              question text, answer options, and the correct answer index.
  */
 
 public class Question {
@@ -20,12 +19,14 @@ public class Question {
     // The index of the correct answer in the options array
     private int correctIndex;
 
+
     // Constructor: Creates a question with text, options, and the correct index
     public Question(String prompt, String[] options, int correctIndex) {
         this.prompt = prompt;
         this.options = options;
         this.correctIndex = correctIndex;
     }
+
 
     // Constructor: Creates an empty question placeholder
     public Question() {
@@ -34,33 +35,45 @@ public class Question {
         this.correctIndex = -1;
     }
 
+
     // Returns the question text
     public String getPrompt() {
         return prompt;
     }
+
 
     // Sets the question text
     public void setPrompt(String prompt) {
         this.prompt = prompt;
     }
 
+
     // Returns the array of answer choices
     public String[] getOptions() {
         return options;
     }
+
 
     // Sets the answer choices
     public void setOptions(String[] options) {
         this.options = options;
     }
 
+
     // Returns the index of the correct answer
     public int getCorrectIndex() {
         return correctIndex;
     }
 
+
     // Sets the index of the correct answer
     public void setCorrectIndex(int correctIndex) {
         this.correctIndex = correctIndex;
+    }
+
+
+    // Checks whether a selected answer index is correct
+    public boolean isCorrect(int selectedIndex) {
+        return selectedIndex == correctIndex;
     }
 }
